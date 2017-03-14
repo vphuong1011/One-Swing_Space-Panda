23274c968caf32ed411d70599aef159e521823c6
Phuong Pham Viet <vphuong10111@gmail.com>
2017-03-14 12:35:03 -0700

    Work in progress

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BounceArrowC : MonoBehaviour {
+public class Bullet : MonoBehaviour {
 
 	private float speed = 5;
 
@@ -10,13 +10,13 @@ public class BounceArrowC : MonoBehaviour {
     }
 	// Use this for initialization
 	void Start () {
+        
 	}
 
 
 	// Update is called once per frame
 	void Update () {
 		transform.Translate (Vector3.right * Time.deltaTime * speed);
-
 	}
 	void OnCollisionEnter (Collision col)
 	{
@@ -24,8 +24,8 @@ public class BounceArrowC : MonoBehaviour {
         {
             foreach (ContactPoint contact in col.contacts)
             {
-                Vector3 reflectDir = Vector3.Reflect(col.gameObject.transform.position, Vector3.right);
-                float rot = 90 - Mathf.Atan2(reflectDir.y, reflectDir.x) * Mathf.Rad2Deg;
+                Vector3 reflectDir = Vector3.Reflect(col.gameObject.transform.position, -Vector3.forward);
+                float rot = -180 - Mathf.Atan2(reflectDir.x, reflectDir.z) * Mathf.Rad2Deg;
                 transform.eulerAngles = new Vector3(0, 0, rot);
             }     
         }
