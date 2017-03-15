98b4aaf5a74f65d38ac8519a0410417805c88baa
Phuong Pham Viet <vphuong10111@gmail.com>
2017-03-15 00:42:46 -0700

    Updated

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -3,14 +3,15 @@ using System.Collections;
 
 public class Bullet : MonoBehaviour {
 
-	private float speed = 5;
+	private float speed = 10;
 
+    Collider col;
     public void Awake()
     {
     }
 	// Use this for initialization
 	void Start () {
-        
+        col = GetComponent<BoxCollider>();
 	}
 
 
@@ -20,14 +21,36 @@ public class Bullet : MonoBehaviour {
 	}
 	void OnCollisionEnter (Collision col)
 	{
-        if(gameObject.name == "Bullet")
-        {
-            foreach (ContactPoint contact in col.contacts)
-            {
+        if(col.gameObject.name == "Player")
+        {    
                 Vector3 reflectDir = Vector3.Reflect(col.gameObject.transform.position, -Vector3.forward);
                 float rot = -180 - Mathf.Atan2(reflectDir.x, reflectDir.z) * Mathf.Rad2Deg;
-                transform.eulerAngles = new Vector3(0, 0, rot);
-            }     
+                transform.eulerAngles = new Vector3(0, 0, rot);               
         }
+
+        if(col.gameObject.tag == "Prop")
+        {
+            GoThrough();
+            Debug.Log("Go");
+        }
+    }
+
+    private void OnCollisionExit(Collision col)
+    {
+        if(col.gameObject.tag == "Prop")
+        {
+            StopGoingThrough();
+            Debug.Log("Stop");
+        }
+    }
+
+    void GoThrough()
+    {
+        col.isTrigger = true;
+    }
+
+    void StopGoingThrough()
+    {
+        col.isTrigger = false;
     }
 }
---
+++ Assets/DestroyWhenTouch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWhenTouch : MonoBehaviour {
+   public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "Bullet")
+        {
+            EnableRagdoll();
+        }
+    }
+    void EnableRagdoll()
+    {
+        rb.isKinematic = false;
+        rb.detectCollisions = true;
+    }
+
+    void DisableRagdoll()
+    {
+        rb.isKinematic = true;
+        rb.detectCollisions = false;
+    }
+}
