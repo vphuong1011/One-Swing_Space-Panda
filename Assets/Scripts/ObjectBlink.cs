2f1b7600f840235f4c83196c5bfe6cbe1d965b55
Phuong Pham Viet <vphuong10111@gmail.com>
2017-03-22 18:57:20 -0700

    Core gameplay

---
+++ Assets/Scripts/ObjectBlink.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectBlink : MonoBehaviour {
+
+    public Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+    }
+    // Update is called once per frame
+    void Update () {
+        InvokeRepeating("Blink", 0f, 1f);
+    }
+
+    IEnumerator Blink()
+    {
+        rend.enabled = false;
+        yield return new WaitForSeconds(0.2f);
+        rend.enabled = true;
+    }
+}
